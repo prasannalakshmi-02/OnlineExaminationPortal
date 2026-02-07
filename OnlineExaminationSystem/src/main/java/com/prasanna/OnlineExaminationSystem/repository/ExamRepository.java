3130ff58306f11bca5ae24e7145b28ff596000ea
prasannalakshmi-02 <prasannalakshmimotati@gmail.com>
2026-02-07 21:01:45 +0530

    Delete OnlineExaminationSystem directory

--- OnlineExaminationSystem/src/main/java/com/prasanna/OnlineExaminationSystem/repository/ExamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prasanna.OnlineExaminationSystem.repository;
-
-import com.prasanna.OnlineExaminationSystem.Entity.Exam;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamRepository extends JpaRepository<Exam, Long> {
-}
--- OnlineExaminationSystem/src/main/java/com/prasanna/OnlineExaminationSystem/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.prasanna.OnlineExaminationSystem.service;
-
-import com.prasanna.OnlineExaminationSystem.Entity.User;
-
-public interface UserService {
-
-    User saveUser(User user);
-    User findByUsername(String username);
-}
